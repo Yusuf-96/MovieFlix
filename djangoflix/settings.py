4a3031a43a2a8302408077da8cfa9d8551c929b5
yusuph <yousufummar649@gmail.com>
2021-05-24 23:24:27 +0300

    playlist commit

--- djangoflix/settings.py
+++ djangoflix/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #Custom Apps
-    'videos',
+    'videos', 'playlists'
 ]
 
 MIDDLEWARE = [
--- playlists/models.py
+++ playlists/models.py
@@ -2,19 +2,16 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.text import slugify
-
+from videos.models import Video
 # Create your models here.
 
 
 class PublishStateOptions(models.TextChoices):
-    # CONSTANT = DB_VALUE, USER_DISPLAY_VALUE
     PUBLISH = 'PU', 'Publish'
     DRAFT = 'DR', 'Draft'
-    # UNLISTED = 'UN', 'Unlisted'
-    # PRIVATE = 'PR', 'Private'
 
 
-class VideoQuerySet(models.QuerySet):
+class PlaylistQuerySet(models.QuerySet):
     def published(self):
         now = time
         return self.filter(
@@ -23,28 +20,28 @@ class VideoQuerySet(models.QuerySet):
         )
 
 
-class VideoManager(models.Manager):
+class PlaylistManager(models.Manager):
     def get_queryset(self):
-        return VideoQuerySet(self.model, using=self._db)
+        return PlaylistQuerySet(self.model, using=self._db)
 
     def published(self):
         return self.get_queryset().published()
 
 
-class Video(models.Model):
+class Playlist(models.Model):
     title = models.CharField(max_length=200)
-    desctiption = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    video = models.ForeignKey(Video, null=True, on_delete=models.SET_NULL)
     slug = models.SlugField(blank=True, null=True)
-    video_id = models.CharField(max_length=200, unique=True)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
     state = models.CharField(
         max_length=2, choices=PublishStateOptions.choices, default=PublishStateOptions.DRAFT)
     publish_timestamp = models.DateTimeField(
         auto_now_add=False, auto_now=False, blank=True, null=True)
 
-    objects = VideoManager()
+    objects = PlaylistManager()
 
     @property
     def is_published(self):
@@ -52,40 +49,18 @@ class Video(models.Model):
 
     def save(self, *args, **kwargs):
         if self.slug is None:
-            self.slug = slugify(self.title)
+            self.slug = slufgify(self.title)
         super().save(*args, **kwargs)
 
-    def get_playlist_ids(sefl):
-        return self.playlist_set.all().values_list('id', flat = True)
-
-
-class videoAllProxy(Video):
-    class Meta:
-        proxy = True
-        verbose_name = 'All video'
-        verbose_name_plural = 'All Videos'
-
-
-class VideoPublishedProxy(Video):
-    class Meta:
-        proxy = True
-
-        verbose_name = 'Published Video'
-        verbose_name_plural = 'Published Videos'
-
-
 def publish_state_pre_save(sender, instance, *args, **kwargs):
-    is_publish = instance.state == instance.PublishStateOptions.PUBLISH
+    is_publishe = instance.state == instance.PublishStateOptions.PUBLISH
     is_draft = instance.state == instance.PublishStateOptions.DRAFT
     if is_publish and instance.publish_timestamp is None:
-        print("save as timestamp for published")
-        instance.publish_timestamp = timezono.now()
+        instance.publish_timestamp = timezone.now()
     elif is_draft:
         instance.publish_timestamp = None
 
-
-pre_save.connect(publish_state_pre_save, sender=Video)
-
+pre_save.connect(publish_state_pre_save, sender = Playlist)
 
 def slugify_pre_save(sender, instance, *args, **kwargs):
     title = instance.title
@@ -93,5 +68,5 @@ def slugify_pre_save(sender, instance, *args, **kwargs):
     if slug is None:
         instance.slug = slugify(title)
 
-
-pre_save.connect(slugify_pre_save, sender=Video)
+pre_save.connect(slugify_pre_save, sender = Playlist)
+        
